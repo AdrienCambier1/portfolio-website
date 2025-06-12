c6c01dddead48bdf99652313fbbad426c58f953a
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-06-12 21:55:03 +0200

    ...

--- src/Page/Header.tsx
+++ src/Page/Header.tsx
@@ -20,91 +20,101 @@ export default function Header() {
   }, [menu]);
 
   return (
-    <header className="menu">
-      <ScrollLink to="/" id="top" className="name-header">
-        ADRIEN CAMBIER
-      </ScrollLink>
-      <nav className={`nav-links scrollbar-hide ${menu && "menu-on"}`}>
-        <div className="name-menu">
-          <p className="name-menu-text">
-            <ScrollLink to="/" id="top" onClick={() => setMenu(!menu)}>
-              ADRIEN CAMBIER
-            </ScrollLink>
-          </p>
-          <img className="close" src={Close} onClick={() => setMenu(!menu)} />
-        </div>
-        <ul>
-          <li>
-            <ScrollLink
-              className="links"
-              to="/"
-              id="propos"
-              onClick={() => setMenu(false)}
-            >
-              {traductions[selectedLanguage as "fr" | "en"]["À propos de moi"]}
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              className="links"
-              to="/"
-              id="skills"
-              onClick={() => setMenu(false)}
-            >
-              {traductions[selectedLanguage as "fr" | "en"]["Mes compétences"]}
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              className="links"
-              to="/"
-              id="parcours"
-              onClick={() => setMenu(false)}
-            >
-              {traductions[selectedLanguage as "fr" | "en"]["Mon parcours"]}
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              className="links"
-              to="/"
-              id="portfolio"
-              onClick={() => setMenu(false)}
-            >
-              {traductions[selectedLanguage as "fr" | "en"]["Mon portfolio"]}
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              className="links"
-              to="/"
-              id="loisirs"
-              onClick={() => setMenu(false)}
-            >
-              {traductions[selectedLanguage as "fr" | "en"]["Mes loisirs"]}
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              className="links"
-              to="/"
-              id="contact"
-              onClick={() => setMenu(false)}
-            >
-              {traductions[selectedLanguage as "fr" | "en"]["Me contacter"]}
-            </ScrollLink>
-          </li>
-        </ul>
-      </nav>
-      {menu && (
-        <div className="background-nav" onClick={() => setMenu(!menu)} />
-      )}
+    <header>
+      <div className="menu">
+        <ScrollLink to="/" id="top" className="name-header">
+          ADRIEN CAMBIER
+        </ScrollLink>
+        <nav className={`nav-links scrollbar-hide ${menu && "menu-on"}`}>
+          <div className="name-menu">
+            <p className="name-menu-text">
+              <ScrollLink to="/" id="top" onClick={() => setMenu(!menu)}>
+                ADRIEN CAMBIER
+              </ScrollLink>
+            </p>
+            <img className="close" src={Close} onClick={() => setMenu(!menu)} />
+          </div>
+          <ul>
+            <li>
+              <ScrollLink
+                className="links"
+                to="/"
+                id="propos"
+                onClick={() => setMenu(false)}
+              >
+                {
+                  traductions[selectedLanguage as "fr" | "en"][
+                    "À propos de moi"
+                  ]
+                }
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                className="links"
+                to="/"
+                id="skills"
+                onClick={() => setMenu(false)}
+              >
+                {
+                  traductions[selectedLanguage as "fr" | "en"][
+                    "Mes compétences"
+                  ]
+                }
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                className="links"
+                to="/"
+                id="parcours"
+                onClick={() => setMenu(false)}
+              >
+                {traductions[selectedLanguage as "fr" | "en"]["Mon parcours"]}
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                className="links"
+                to="/"
+                id="portfolio"
+                onClick={() => setMenu(false)}
+              >
+                {traductions[selectedLanguage as "fr" | "en"]["Mon portfolio"]}
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                className="links"
+                to="/"
+                id="loisirs"
+                onClick={() => setMenu(false)}
+              >
+                {traductions[selectedLanguage as "fr" | "en"]["Mes loisirs"]}
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                className="links"
+                to="/"
+                id="contact"
+                onClick={() => setMenu(false)}
+              >
+                {traductions[selectedLanguage as "fr" | "en"]["Me contacter"]}
+              </ScrollLink>
+            </li>
+          </ul>
+        </nav>
+        {menu && (
+          <div className="background-nav" onClick={() => setMenu(!menu)} />
+        )}
 
-      <img
-        className="hamburger-menu"
-        src={Hamburger}
-        onClick={() => setMenu(!menu)}
-      />
+        <img
+          className="hamburger-menu"
+          src={Hamburger}
+          onClick={() => setMenu(!menu)}
+        />
+      </div>
     </header>
   );
 }

1e6fa4f6b99c48fbc65760c2e20918f7b129bdcc
Adrien Cambier <adriencambier04@gmail.com>
2026-03-13 17:09:42 +0100

    Add Vercel Analytics for improved performance tracking

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./Style/style.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <Analytics />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
